fb645309891b6b094e01d61389a85d8b356b72fe
Nayana Mukherjee <148554152+43nayana@users.noreply.github.com>
2025-07-07 22:42:08 +0530

    Update ALU_8bit.v

--- ALU_8bit.v
+++ ALU_8bit.v
@@ -1,1 +1,21 @@
+module ALU(
+    input [7:0] a, b,
+    input [3:0] operation,
+    output reg [8:0] z
+);
 
+always @(*) begin
+    case(operation)
+        4'b0000: z = a + b;
+        4'b0001: z = a - b;
+        4'b0010: z = (b != 0) ? a / b : 9'b0; // handle divide-by-zero
+        4'b0011: z = a * b;
+        4'b0100: z = a & b;
+        4'b0101: z = a | b;
+        4'b0110: z = a ^ b;
+        4'b0111: z = ~a;
+        4'b1000: z = a << 1;
+        4'b1001: z = a >> 1;
+        default: z = 9'b000000000;
+    endcase
+end
